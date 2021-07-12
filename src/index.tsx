4619849cb08ca880e97ebcb5d99c40dec1441021
Andrey <shock.work@mail.ru>
2021-07-12 20:13:18 +0300

    add bootstrap-icons

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 ReactDOM.render(
   <React.StrictMode>
