610205e86bddd14d3a8b42fcdba0fa3ce482b146
Andrey <shock.work@mail.ru>
2021-07-12 23:28:09 +0300

    install concurrently to start 2 commands, change url api

--- src/core/axios.ts
+++ src/core/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://my-json-server.typicode.com/shock-dev/goodbit-posts/'
+  baseURL: 'http://localhost:5000/'
 });
 
 export default instance;
